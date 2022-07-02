06dfc2420a67dbdbd08d13345e85eced0ee38461
berkayonall <berkayonal@hotmail.com>
2022-07-02 23:40:41 +0300

    Initial commit

---
+++ src/Fibo.java
@@ -0,0 +1,15 @@
+public class Fibo {
+    static int fibo(int a){
+
+            if (a==1||a==2){
+                return 1;
+            }
+
+
+        return fibo(a -1)+fibo(a-2);
+    }
+    public static void main(String[] args) {
+        System.out.println(fibo(5));
+
+    }
+}
